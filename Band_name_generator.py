279aa6d86aa1d27dcc67d6ae13a7685a18aa9e27
Delightai <98855713+Delightai@users.noreply.github.com>
2024-01-24 21:45:48 +0100

    this is a python program to generate a band name

---
+++ Band_name_generator.py
@@ -0,0 +1,8 @@
+print ("Welcome to the band generator")
+city = input("Can you please let me know the city you grew up in ?\n")
+pet_name = input("What is your pet name? \n")
+
+band_name = city + pet_name
+
+print ("Your band name could be " + str(band_name))
+
